3d175d864ea37ab313cca67585d3ae391801ffdf
puneeth2001 <puneethchanda2001@gmail.com>
2019-08-24 16:02:38 +0530

    Find MAxx Depth of a Binary Tree

--- LeetCode/MaxDepth.java
+++ LeetCode/MaxDepth.java
@@ -1,3 +1,18 @@
 class MaxDepth{
-    
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        else{
+            return 1+max(maxDepth(root.left), maxDepth(root.right));
+        }
+    }
+    public int max(int a, int b){
+        if(a>b){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
 }
