347a2154f8b733fb040a50c0bffdddc8d7655bcf
puneeth2001 <puneethchanda2001@gmail.com>
2019-08-26 20:18:51 +0530

    Search

---
+++ LeetCode/Search.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null ) {
+			return null;
+		}
+		else if(root.val == val) {
+			return root;
+		}
+		else if(val<root.val) {
+			return searchBST(root.left,val);
+		}
+		else if(val>root.val) {
+			return searchBST(root.right,val);
+		}
+return root;
+    }
+}
